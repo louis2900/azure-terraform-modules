e4e18c348dbc4c60082d88e19a0eb2d7803b6962
louis <louis-florian@hotmail.com>
2025-09-28 17:26:17 +0200

    appended feature

--- modules/azure_storage_account/main.tf
+++ modules/azure_storage_account/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_storage_account" "this" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
-  account_tier             = var.account_tiere
+  account_tier             = var.account_tier
   access_tier              = var.access_tier
   account_replication_type = var.account_replication_type
   tags                     = var.tags
